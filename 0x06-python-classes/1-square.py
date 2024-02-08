b50144810b9875261c5ba1fcaa9ce87c13d68048
Fomonyuy-Claire <fomonyuymarie2000@gmail.com>
2024-02-07 17:59:28 -0800

    python classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Class Square declaration"""
+
+
+class Square:
+    """My square class"""
+
+    def __init__(self, size):
+        """My Square initialization"""
+        self.__size = size
